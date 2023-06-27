597ea283ae0ae0ab456600fb4ff1cbdd665ee403
Abdul Gamal <abdul.nanjira@gmail.com>
2023-06-27 17:25:33 +0300

    footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,12 @@ function Footer() {
   return (
     <footer className="bg-white md:rounded-lg shadow m-4 container mx-auto">
       <div className="w-full mx-auto max-w-screen-xl p-4 flex items-center justify-center">
-        <span className="text-sm text-gray-500 sm:text-center">
+        <span className="text-sm text-gray-500 text-center">
           © 2023{" "}
           <a href="#" className="hover:underline">
             NiPlug™
-          </a>
-          . All Rights Reserved.
+          </a>{" "}
+          by Paysoko Systems. All Rights Reserved.
         </span>
       </div>
     </footer>
