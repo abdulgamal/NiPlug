37614017e1f1be7c0dadc290ae7010a9529825d5
abdulgamal <gamala@paysokosystems.com>
2025-08-12 16:37:29 +0300

    fix: social icons

--- src/components/StudioNav.jsx
+++ src/components/StudioNav.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaInstagram, FaTiktok, FaYoutube } from "react-icons/fa";
 
 function StudioNav() {
   return (
@@ -7,9 +8,9 @@ function StudioNav() {
         <div className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src="logo.png" className="h-8" alt="Niplug Logo" />
         </div>
-        <div className="flex space-x-3 ">
+        <div className="flex space-x-3 items-center">
           <div
-            className="items-center justify-between hidden w-full md:flex md:w-auto"
+            className="items-center justify-between hidden w-full md:flex md:w-auto mr-10"
             id="navbar-sticky"
           >
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
@@ -30,22 +31,40 @@ function StudioNav() {
                   Services
                 </a>
               </li>
-              {/* <li>
-                <a
-                  href="#academy"
-                  className="block py-2 px-3 text-white rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-teal-700 md:p-0"
-                >
-                  Academy
-                </a>
-              </li> */}
             </ul>
           </div>
-          <a
+          <div className="flex space-x-4">
+            <a
+              href="https://www.instagram.com/ni.plug?igsh=MTM3dXZtZ3Q4bTQ4dg%3D%3D&utm_source=qr"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white hover:text-teal-700"
+            >
+              <FaInstagram className="w-6 h-6" />
+            </a>
+            <a
+              href="https://www.youtube.com/@NiPlugMedia/videos"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white hover:text-teal-700"
+            >
+              <FaYoutube className="w-6 h-6" />
+            </a>
+            <a
+              href="https://www.tiktok.com/@ni.plug?_t=ZM-8yoOUgOu6OT&_r=1"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white hover:text-teal-700"
+            >
+              <FaTiktok className="w-6 h-6" />
+            </a>
+          </div>
+          {/* <a
             href="#contacts"
             className="text-teal-600 bg-[#FBF2B8] hover:bg-[#FBF2B8] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center"
           >
             Contact Us
-          </a>
+          </a> */}
         </div>
       </div>
     </nav>
